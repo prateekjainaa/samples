4c6c7c51f0d1f056e22a95cfa94f47124407893f
Prateek Jain <pjain@localhost.localdomain>
2015-04-15 22:41:25 +0530

    Trying to create new project

---
+++ jee7/servlets/src/main/java/org/pjain/jee7/servlets/MyServlet.java
@@ -0,0 +1,23 @@
+package org.pjain.jee7.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns={"/pjain/serv1"})
+public class MyServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.write("Hello! World");
+	}
+	
+}
